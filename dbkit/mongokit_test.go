901ded914a5ed9879b34dc7edb83cd90ee1a60f9
lijinyu <JackRipper_1888@163.com>
2020-05-13 16:46:00 +0800

    "mogo_kit"

--- dbkit/mongokit_test.go
+++ dbkit/mongokit_test.go
@@ -1,50 +1,47 @@
-package utils
+package dbkit
 
 import (
 	"fmt"
-	"gopkg.in/mgo.v2/bson"
+	"github.com/Ripper/ctxkit"
 	"testing"
 )
 
 const (
 	USER = "user"
 	//[mongodb://][user:pass@]host1[:port1][,host2[:port2],...][/database][?options]
-	MONGO_URL   = "mongodb://yanfa:ff2019yqcsdew@183.60.143.82:36068"
-	DBNAME      = "p2p_state"
-	colelection = "p2p"
-	MAX_CONN    = 0
+	MONGO_URL = "mongodb://yanfa:ff2019yqcsdew@183.60.143.82:36068"
+	DBNAME    = "p2p_state"
+	MAX_CONN  = 0
 )
 
-type UserInfo struct {
-	Name      string     `bson:"name"`
-	AllStream []string   `bson:"all_stream"`
-	Loc       [2]float32 `bson:"loc"`
-}
-
 func TestMongoDB(t *testing.T) {
+	//释放心跳协程
+	defer ctxkit.CancelAll()
 	fmt.Println("TestMongoDB start")
 	InitMongoDB(USER, MONGO_URL, DBNAME, MAX_CONN)
-	//data := UserInfo{
-	//	Name: "王多余",
-	//	AllStream:[]string{"wush","wanming"},
-	//	Loc: [2]float32{12.33, 44.55},
-	//}
-	//err := GetMongoConn(USER).Insert(colelection,data)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	Geodata, err := GetMongoConn(USER).FindGeoNear(colelection, 12.00, 13.00, 1)
+	conn := GetMongoConn(USER)
+
+	params := map[string]interface{}{"uid": "asdhasianjkaihs"}
+	document := map[string]interface{}{
+		//"name":"王大锤",
+		"all_stream": []map[string]interface{}{
+			{"streamid": "wusha",
+				"resourceurl":  "12g3h1ihd",
+				"resourcename": "误杀",
+			},
+		},
+		"loc": [2]float32{100.00, 87.55},
+	}
+	err := conn.Update("p2p", document, params)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	if Geodata == nil {
-		fmt.Println("reading", Geodata)
+
+	Geodata, err := conn.FindGeoNear("p2p", 100.00, 13.00, 100, params)
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-	fmt.Println("reading")
-	for _, v := range Geodata.([]interface{}) {
-		fmt.Println(v.(bson.M)["obj"].(bson.M)["loc"])
-	}
+	fmt.Println("reading", Geodata)
 }
--- dbkit/rediskit.go
+++ dbkit/rediskit.go
@@ -1,4 +1,4 @@
-package utils
+package dbkit
 
 import (
 	"fmt"
