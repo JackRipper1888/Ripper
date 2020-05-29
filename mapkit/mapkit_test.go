66d5399325eb482a13132695bb93ee6cf0276253
lijinyu <JackRipper_1888@163.com>
2020-05-29 11:59:33 +0800

    "mapkit"

--- mapkit/mapkit_test.go
+++ mapkit/mapkit_test.go
@@ -31,6 +31,22 @@ func BenchmarkLock(b *testing.B) {
 	}
 }
 
+//go test -v -bench=BenchmarkRangeMap -benchmem mapkit_test.go mapkit.go
+func BenchmarkRangeMap(b *testing.B) {
+	m := NewConcurrentMap(64)
+	var n int
+	for i := 0; i < b.N; i++ {
+		n++
+		m.Set(strconv.Itoa(i), i)
+	}
+
+	b.StartTimer()
+	m.Range(func(k string, v interface{}) bool {
+		return true
+	})
+	b.StopTimer()
+}
+
 //go:generata
 //go test -v -bench=BenchmarkConcurrentMap -benchmem mapkit_test.go mapkit.go
 func BenchmarkConcurrentMap(b *testing.B) {
@@ -63,6 +79,9 @@ func BenchmarkSyncMap(b *testing.B) {
 	if isExist {
 		b.Logf("sync map%d个数据中查找%d", n, v.(int))
 	}
+	//m.Range(func(k, v interface{})bool{
+	//	return true
+	//})
 	b.StopTimer()
 }
 
